bc913f6ac473865e63dfb88402d20bb8a1f53387
matvki <m.vecchi@it-students.fr>
2021-11-12 15:09:30 +0100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# php_moyenne
+# Php_moyenne readme
+Ne pas oublier de générer la base de donnée, et de modifier les informations dans connectDB.php
+
+# Générer la BDD
+```SQL
+create table if not exists eleve
+(
+	id int auto_increment
+		primary key,
+	name varchar(255) not null
+);
+
+create table if not exists note
+(
+	id int auto_increment,
+	value int not null,
+	eleve_id int not null,
+	constraint note_id_uindex
+		unique (id)
+);
+
+alter table note
+	add primary key (id);
+
+
+```
--- connectDB.php
+++ connectDB.php
@@ -1,31 +1,11 @@
 <?php
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = 'root';
-$db_db = 'moyenne';
-$db_port = 8889;
 try {
     $db = new PDO('mysql:host=localhost;port=8889;dbname=moyenne',
         'root',
         'root'
-    );}
+    );} // connection bdd
 catch(Exception $e)
 {
     // En cas d'erreur, on affiche un message et on arrête tout
     die('Erreur : '.$e->getMessage());
 }
-//
-//if ($mysqli->connect_error) {
-//    echo 'Errno: '.$mysqli->connect_errno;
-//    echo '<br>';
-//    echo 'Error: '.$mysqli->connect_error;
-//    exit();
-//}
-//
-//echo 'Success: A proper connection to MySQL was made.';
-//echo '<br>';
-//echo 'Host information: '.$mysqli->host_info;
-//echo '<br>';
-//echo 'Protocol version: '.$mysqli->protocol_version;
-//
-//$mysqli->close();
